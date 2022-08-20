ec1f9d25c4c811460c793de02bb8d566eca1e579
Damian Demasi <damian.demasi.1@gmail.com>
2022-08-20 19:04:43 +0930

    Listing 2.1

---
+++ listing_2_1.rb
@@ -0,0 +1,13 @@
+chainring = 52 # Number of teeth
+cog = 11
+ratio = chainring / cog.to_f
+p ratio
+
+chainring = 30
+cog = 27
+ratio = chainring / cog.to_f
+p ratio
+
+# ❯ ruby listing_2_1.rb 
+# 4.7272727272727275
+# 1.1111111111111112
